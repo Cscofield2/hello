23db20f6c9a4c6f903dd098379449a44afbfa94e
cscofield2 <cscofield2@huskers.unl.edu>
2025-08-25 23:42:27 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Carson Scofield
+ *         cscofield2@huskers.unl.edu
+ * Date: 8/25/2025
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+ printf("Carson Scofield!\n");
+ printf("Computer Engineering\n")
+
+ return 0;
+}
