6fab69258099326dc60cdeb3101716433d896694
Cscofield2 <cscofield2@huskers.unl.edu>
2025-09-19 19:57:21 +0000

    Fri, Sep 19, 2025, 2:57 PM -05:00

--- retire.c
+++ retire.c
@@ -43,13 +43,15 @@ if (argc != 6){
     startingBalance += monthlyDeposit + interest;
     totalInterest += interest;
     totalBalance = startingBalance;
-    printf("%d $        %.2lf         %.2lf\n", i, interest, startingBalance);
+    printf("%d $        %.2lf $        %.2lf\n", i, interest, startingBalance);
     }
 }else {
     printf("Monthly contributions exceed $18,500 annual limit\n");
     return 1;
 }
-    totalInterest = round(totalInterest )
+    //totalInterest = round(totalInterest * 100) /100;
+   // totalBalance = round(totalBalance * 100) /100;
+
     printf("Total Interest Earned: $ %.2lf\n", totalInterest);
     printf("Total Balance: $ %.2lf\n", totalBalance);
 
